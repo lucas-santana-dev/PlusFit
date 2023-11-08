77e9c350033fcebdaf48ededf56ec7c74d31809e
Lucas Santana <lucassantana@sgasistemas.com.br>
2023-11-08 11:56:24 -0300

    Implementação da lib navigation-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
 
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
 
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
@@ -63,7 +64,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-
+    implementation( "androidx.navigation:navigation-compose:2.7.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
